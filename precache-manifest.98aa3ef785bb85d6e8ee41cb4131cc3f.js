eff704830d14df981461ebc3f15e2214732c2c05
Guillermo Lara <grlara9@gmail.com>
2020-06-18 11:52:00 -0700

    Updates

---
+++ precache-manifest.98aa3ef785bb85d6e8ee41cb4131cc3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f112a7cafb0a31e054dc2cfbb855edd3",
+    "url": "/react_stopwatch/index.html"
+  },
+  {
+    "revision": "35c7a64d270e27618fe9",
+    "url": "/react_stopwatch/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0373c93e0714730d7bcd",
+    "url": "/react_stopwatch/static/css/main.12b5fb41.chunk.css"
+  },
+  {
+    "revision": "35c7a64d270e27618fe9",
+    "url": "/react_stopwatch/static/js/2.a3c29bdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_stopwatch/static/js/2.a3c29bdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0373c93e0714730d7bcd",
+    "url": "/react_stopwatch/static/js/main.874d435f.chunk.js"
+  },
+  {
+    "revision": "f90f565178710e234f6e",
+    "url": "/react_stopwatch/static/js/runtime-main.c500064b.js"
+  }
+]);
